dee600825d985a1bd88153fca2d893b09ba3deb8
VyskeyK <robintyan96@gmail.com>
2023-10-09 21:42:15 +0300

    c

--- catalog/models.py
+++ catalog/models.py
@@ -5,8 +5,31 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Product (models, Model):
-    pass
+
+    name = models.CharField(max.lenght = 200, verbos_name = 'наименование')
+    description = models.TextField(**NULLABLE, verbos_name = 'описание')
+    image(preview) = models.ImageField(upload = 'catalog', **NULLABLE, verbos_name = 'изображение')
+    tip = models.CharField(max.lenght = 200, verbos_name = 'категория')
+    purchase_price = models.IntegerField(**NULLABLE, verbos_name = 'цена за покупку')
+    date_of_creation = models.DateField(**NULLABLE, verbos_name = 'дата создания')
+    date_of_last_modification = models.DateField(**NULLABLE, verbos_name = 'дата последнего изменения')
+
+    def __str__(self):
+        return  f'{self.name}({self.tip})'
+
+
+    class Meta:
+
+        verbos_name = 'Продукт'
+        verbos_name_plural = 'Продукты'
+
+
 
 
 class Category (models, Model):
-    pass
+
+    name_category = models.CharField(max.lenght = 200, verbos_name = 'наименование категории')
+    description_category = models.TextField(**NULLABLE, verbos_name='описание категории')
+
+
+
