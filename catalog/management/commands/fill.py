f174aecdc73195aaf739a5b840f41235698962e1
Vasilii Kuptsov <kuptsovy@mail.ru>
2023-10-17 23:31:51 +0300

    2

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,5 +1,6 @@
 from django.core.management import BaseCommand
-from catalog.models import  Category
+from catalog.models import  Category, Product
+from datetime import datetime
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
@@ -13,4 +14,21 @@ class Command(BaseCommand):
             category_for_create.append(Category(**item))
 
         Category.objects.bulk_create(category_for_create)
+
+
+product_list = [
+    {"name": "окунь", "description": "морской", "image": "", "category": "рыба", "purchase_price": 800,
+     "data_of_creation": "11-10-2023", "data_of_last_modification": "2023-10-11"},
+    {"name": "свинина", "description": "мясо", "image": "", "category": "мясо", "purchase_price": 800,
+     "data_of_creation": "2023-10-11", "data_of_last_modification": "2023-10-11"}
+]
+
+product_for_create = []
+for product in product_list:
+    category_name = product.get('category')
+    category = Category.objects.get(name=category_name)
+    product['category'] = category
+    product_for_create.append(Product(**product))
+
+Product.objects.bulk_create(product_for_create)
 
