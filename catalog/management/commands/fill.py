528f12df5e1bc98deef0ee7d363c465781e767f4
Vasilii Kuptsov <kuptsovy@mail.ru>
2023-10-19 22:44:18 +0300

    4

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -16,19 +16,19 @@ class Command(BaseCommand):
         Category.objects.bulk_create(category_for_create)
 
 
-product_list = [
-    {"name": "окунь", "description": "морской", "image": "", "category": "рыба", "purchase_price": 800,
-     "date_of_creation": "2023-10-11", "date_of_last_modification": "2023-10-11"},
-    {"name": "свинина", "description": "мясо", "image": "", "category": "мясо", "purchase_price": 800,
-     "date_of_creation": "2023-10-11", "date_of_last_modification": "2023-10-11"}
-]
+        product_list = [
+            {"name": "окунь", "description": "морской", "image": "", "category": "рыба", "purchase_price": 800,
+            "date_of_creation": "2023-10-11", "date_of_last_modification": "2023-10-11"},
+            {"name": "свинина", "description": "мясо", "image": "", "category": "мясо", "purchase_price": 800,
+            "date_of_creation": "2023-10-11", "date_of_last_modification": "2023-10-11"}
+        ]
 
-product_for_create = []
-for product in product_list:
-    category_name = product.get('category')
-    category = Category.objects.get(name=category_name)
-    product['category'] = category
-    product_for_create.append(Product(**product))
-
-Product.objects.bulk_create(product_for_create)
+        product_for_create = []
+        for product in product_list:
+            category_name = product.get('category')
+            category = Category.objects.get(name=category_name)
+            product['category'] = category
+            product_for_create.append(Product(**product))
+        print(product_for_create)
+        Product.objects.bulk_create(product_for_create)
 
